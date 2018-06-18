ec5ce9e4feca694d336042e7a453d7d9f8ef7826
isr10132 <isr10132@gmail.com>
2018-06-18 17:34:26 +0800

    Update calendar & debug
    EventDrop
    
    EventClick
    
    TimeFormat
    
    CSS style

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,6 @@
+var tA;
+var tB;
+
 // 記得用檔案引入js	
 $(function(){
 
@@ -8,17 +11,57 @@ $(function(){
         revertDuration: 0  //  original position after the drag
 	});
 
+	// 輸入兩個 time, 若 timeA 較大則回傳 true，否則回傳 false
+	function timeCompare(timeA, timeB)
+	{
+		var arrayA = typeof(timeA) == 'string' ? timeA.split(/-|:|T| |\./) : timeA;
+		var arrayB = typeof(timeB) == 'string' ? timeB.split(/-|:|T| |\./) : timeB;
+		for (var i = 0; i < 6; i++)
+		{
+			a = parseInt(arrayA[i] != undefined ? arrayA[i] : 0);
+			b = parseInt(arrayB[i] != undefined ? arrayB[i] : 0);
+			// console.log(a > b);
+			if (a > b)
+				return true;
+			else if (a < b)
+				return false;
+		}
+		return true;
+	}
+
+	//  2018-05-15 20:30:00.0 -> 2018 年 5 月 15 日 下午 8 點 30 分
+	function toChineseTimeFormat(time, allday)
+	{
+		var array = time.split(/-|:|T| |\./);
+		var format = array[0] + ' 年 ' + ~~array[1] + ' 月 ' + ~~array[2] + ' 日 ';		// ~~可去掉開頭0
+		format += allday == true ? ' 整天' : (array[3] >= 12 ? '下午 ' + ~~(array[3]-12) : '上午 ' + ~~(array[3])) + ' 點' + (array[4] > 0 ? ' ' + ~~array[4] + ' 分' : ' 整');
+		return format;
+	}
+
 	// 取得所有 fc 上的 event
 	$('#get-events-btn').click(function(){
 		var eventsArray = $('#calendar').fullCalendar('clientEvents');
+    	// 將 eventsArray 按照日期排序
+    	eventsArray = eventsArray.sort(function (a, b) {
+    		tA = a;
+    		tB = b;
+        	return timeCompare(a.start.format!=undefined ? a.start.format() : a.start._i, b.start.format!=undefined ? b.start.format() : b.start._i) ? 1 : -1;
+        });
+
+		console.log(eventsArray);
+		$('#output').html('<tr class="table-title"><td>標題</td><td>開始</td><td>結束</td><td>內容</td><td>地點</td></tr>');
 		for (var i = 0; i < eventsArray.length; i++)
 		{
-			console.log('index: ' + i);
-			console.log(eventsArray[i]);
-			console.log('title: ' + eventsArray[i].title);
-			console.log('start: ' + eventsArray[i].start.format());
-			console.log('end: ' + eventsArray[i].end.format());
-			console.log('description: ' + eventsArray[i].description);
+			$('#output').append(
+					'<tr class="table-content" style="background-color: ' + eventsArray[i].backgroundColor + '">' +
+					// '<td>' + i + '</td>' + 
+					'<td>' + eventsArray[i].title + '</td>' +
+					'<td>' + toChineseTimeFormat(eventsArray[i].start.format(), eventsArray[i].allDay) + '</td>' +
+					'<td>' + toChineseTimeFormat(eventsArray[i].end.format(), eventsArray[i].allDay) + '</td>' +
+					'<td>' + eventsArray[i].description + '</td>' +
+					'<td>' + eventsArray[i].position + '</td>' +
+					'</tr>'
+			);
 		}
 	});
 
@@ -38,7 +81,7 @@ $(function(){
 	$('#add-new-event').click(addEvent);
 
 	// 使用者輸入完後 按下 Enter 可視為執行添加
-	$("#new-event-title").on('keyup', function (e) {
+	$("#new-event-title,#new-event-description,#new-event-position").on('keyup', function (e) {
 	    if (e.keyCode == 13) {
 	        addEvent();
 	    }
@@ -48,7 +91,8 @@ $(function(){
 	function addEvent(){
 		$('#selected-event').text($('#new-event-title').val());
 		$('#selected-event').data('description', $('#new-event-description').val());
-		$('#new-event-title,#new-event-description').val('');
+		$('#selected-event').data('position', $('#new-event-position').val());
+		$('#new-event-title,#new-event-description,#new-event-position').val('');
 	}
 
 	$('#calendar').fullCalendar({
@@ -72,21 +116,35 @@ $(function(){
         droppable: true,
         selectable: true,
         selectHelper: true,
-        drop: function(data, allDay) {
+        drop: function(date) {
+        	// console.log(date);
 			var event = {
 				title: $(this).text(),
 	            description: $(this).data('description'),
-	            start: data.toISOString(),
-	            allDay: !data.hasTime(),
+	            position: $('#selected-event').data('position'),
+	            start: date,
+	            allDay: !date.hasTime(),
 	            backgroundColor: $(this).css('background-color'),
 	            borderColor: $(this).css('border-color')
 			}
+    		if (event.allDay != true)
+    		{
+  	    		event.end = moment(event.start);
+	    		event.end.hour(event.end.hour()+2);
+    		}
+    		else
+    		{
+  	    		event.end = moment(event.start);
+  	    		event.end.day(event.end.day()+1);
+    		}
+			// console.log(event);
 			$('#calendar').fullCalendar('renderEvent', event, true);
         },
         select: function(start, end) {
       	    var selectedObject = {
   	      	    title: $('#selected-event').text(),
 	            description: $('#selected-event').data('description'),
+	            position: $('#selected-event').data('position'),
   	      	    start: start.format('YYYY-MM-DD HH:mm:ss'),
   	      	    end: end.format('YYYY-MM-DD HH:mm:ss'),
   	      	    allDay: !start.hasTime() && !end.hasTime(),
@@ -96,6 +154,28 @@ $(function(){
       	    $('#calendar').fullCalendar('renderEvent', selectedObject, true);
       	    $('#calendar').fullCalendar('unselect');
       	},
+      	eventDrop: function(event, delta, revertFunc) {
+  	    	// 若沒有end且不是allday，預設為開始時間 +2 小時
+  	    	if (event.end == null)
+  	    	{
+  	    		if (event.allDay != true)
+  	    		{
+      	    		event.end = moment(event.start);
+      	    		event.end.hour(event.end.hour()+2);
+  	    		}
+  	    		else
+  	    		{
+      	    		event.end = moment(event.start);
+      	    		event.end.day(event.end.day()+1);
+  	    		}
+  	    	}
+  	        $('#calendar').fullCalendar('refetchEvents');
+        }, // end eventDrop
+        eventClick: function(event) {
+        	console.log(event._id);
+        	if (confirm(event.title + '\n\n' + event.description + '\n\n' + event.position + '\n\n\n要刪除嗎？'))
+        		$('#calendar').fullCalendar('removeEvents', event._id);
+        }
     }); // end fullCalendar
 })
 
