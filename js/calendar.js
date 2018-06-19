f37f551c438bcb7d3144e722e39918c94e478376
isr10132 <isr10132@gmail.com>
2018-06-19 16:08:22 +0800

    Integration
    combine index with map & calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,10 +1,6 @@
 var tA;
 var tB;
 var eventsArray = [];
-// 記得用檔案引入js	
-$(function(){
-	initCalendar();
-})
 
 function initCalendar()
 {
@@ -14,24 +10,6 @@ function initCalendar()
         revert: true, // will cause the event to go back to its
         revertDuration: 0  //  original position after the drag
 	});
-
-	// 輸入兩個 time, 若 timeA 較大則回傳 true，否則回傳 false
-	function timeCompare(timeA, timeB)
-	{
-		var arrayA = typeof(timeA) == 'string' ? timeA.split(/-|:|T| |\./) : timeA;
-		var arrayB = typeof(timeB) == 'string' ? timeB.split(/-|:|T| |\./) : timeB;
-		for (var i = 0; i < 6; i++)
-		{
-			a = parseInt(arrayA[i] != undefined ? arrayA[i] : 0);
-			b = parseInt(arrayB[i] != undefined ? arrayB[i] : 0);
-			// console.log(a > b);
-			if (a > b)
-				return true;
-			else if (a < b)
-				return false;
-		}
-		return true;
-	}
 
 	// 取得所有 fc 上的 event
 	$('#get-events-btn').click(function(){
@@ -60,16 +38,6 @@ function initCalendar()
 	        addEvent();
 	    }
 	});
-
-	// 將使用者輸入的資訊弄成 event 並顯示在 selected event
-	function addEvent(){
-		$('#selected-event').text($('#new-event-title').val());
-		$('#selected-event').data('description', $('#new-event-description').val());
-		$('#selected-event').data('position', $('#new-event-position').val());
-		$('#selected-event').data('latitude', LatLng.lat());
-		$('#selected-event').data('longitude', LatLng.lng());
-		$('#new-event-title,#new-event-description,#new-event-position').val('');
-	}
 
 	$('#calendar').fullCalendar({
 		// 視圖的擺放
@@ -170,6 +138,38 @@ function initCalendar()
 	}); 
 }
 
+// 將使用者輸入的資訊弄成 event 並顯示在 selected event
+function addEvent(){
+	console.log($('#new-event-title').val());
+	console.log($('#new-event-description').val());
+	console.log($('#new-event-position').val());
+
+	$('#selected-event').text($('#new-event-title').val());
+	$('#selected-event').data('description', $('#new-event-description').val());
+	$('#selected-event').data('position', $('#new-event-position').val());
+	$('#selected-event').data('latitude', LatLng.lat());
+	$('#selected-event').data('longitude', LatLng.lng());
+	$('#new-event-title,#new-event-description,#new-event-position').val('');
+}
+
+// 輸入兩個 time, 若 timeA 較大則回傳 true，否則回傳 false
+function timeCompare(timeA, timeB)
+{
+	var arrayA = typeof(timeA) == 'string' ? timeA.split(/-|:|T| |\./) : timeA;
+	var arrayB = typeof(timeB) == 'string' ? timeB.split(/-|:|T| |\./) : timeB;
+	for (var i = 0; i < 6; i++)
+	{
+		a = parseInt(arrayA[i] != undefined ? arrayA[i] : 0);
+		b = parseInt(arrayB[i] != undefined ? arrayB[i] : 0);
+		// console.log(a > b);
+		if (a > b)
+			return true;
+		else if (a < b)
+			return false;
+	}
+	return true;
+}
+
 //  2018-05-15 20:30:00.0 -> 2018 年 5 月 15 日 下午 8 點 30 分
 function toChineseTimeFormat(time, allday)
 {
@@ -188,38 +188,3 @@ function setEventsArray()
     	return timeCompare(a.start.format!=undefined ? a.start.format() : a.start._i, b.start.format!=undefined ? b.start.format() : b.start._i) ? 1 : -1;
     });
 }	
-
-function changeToCalendar()
-{
-	var calendarHTML = '<fieldset><legend>Schedule</legend><div class="container"><div class="row"><div class="col-xs-2"><div class="input-group"><input id="new-event-title" type="text" class="form-control"placeholder="Title"></div><div class="input-group"><input id="new-event-description" type="text" class="form-control"placeholder="Description"></div><div class="input-group"><input id="new-event-position" type="text" class="form-control"placeholder="Position"></div><button id="add-new-event" type="button" class="btn btn-primary btn-flat">Add</button></div><div class="col-xs-2"><div id="small-map" class=""></div></div></div></div></fieldset><fieldset><legend>Event</legend><div class=\'external-event\' id=\'selected-event\'>選擇事件...</div><div class="color-btn-group"><button class="red btn color-btn"></button><button class="green btn color-btn"></button><button class="blue btn color-btn"></button></div><div>自訂顏色<input id="color-picker" type="color" value="#00c0ef"></div> </fieldset>';
-	$('#main').html('<div id="calendar"></div>');
-	$('#side').html(calendarHTML);
-	initCalendar();
-	initializeMiniMap();
-	$('#calendar').fullCalendar('addEventSource', eventsArray);
-}
-
-function changeToBigmap()
-{
-	$('#main').html('<div id="map"></div>');
-	$('#side').html('<div id="schedule-area">'
-					 + '<table id="schedule" border="3">'
-					 + '<tbody id="output">'
-					 + '</tbody>'
-					 + '</table></div>');
-	$('#output').html('<tr class="table-title"><td>標題</td><td>開始</td><td>結束</td><td>內容</td><td>地點</td></tr>');
-	for (var i = 0; i < eventsArray.length; i++)
-	{
-		$('#output').append(
-				'<tr class="table-content" style="background-color: ' + eventsArray[i].backgroundColor + '">' +
-				// '<td>' + i + '</td>' + 
-				'<td>' + eventsArray[i].title + '</td>' +
-				'<td>' + toChineseTimeFormat(eventsArray[i].start.format(), eventsArray[i].allDay) + '</td>' +
-				'<td>' + toChineseTimeFormat(eventsArray[i].end.format(), eventsArray[i].allDay) + '</td>' +
-				'<td>' + eventsArray[i].description + '</td>' +
-				'<td><a href="#" onclick="moveToLocation('+i+')">' + eventsArray[i].position + '</a></td>' +
-				'</tr>'
-		);
-	}
-	initMap();
-}
